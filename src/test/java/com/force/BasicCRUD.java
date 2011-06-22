b51f0fc82b9d2e17bed0fefbbdae036037cee46f
Nawab Iqbal <niqbal@salesforce.com>
2011-06-22 03:30:14 -0700

    corrected test case

--- src/test/java/com/force/BasicCRUD.java
+++ src/test/java/com/force/BasicCRUD.java
@@ -2,6 +2,7 @@ package com.force;
 
 
 import com.force.model.Task;
+import junit.framework.Assert;
 import org.junit.Test;
 
 import javax.persistence.EntityManager;
@@ -14,24 +15,18 @@ public class BasicCRUD {
 
     @Test
     public void testCRUD() {
-        //System.setProperty("force.forceDatabase.url","force://login.salesforce.com;user=user;password=password;oauth_key=key_key_key;oauth_secret=secret_secret");
-
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("forceDatabase");
         EntityManager em = emf.createEntityManager();
         EntityTransaction tx = em.getTransaction();
         tx.begin();
-
-        Task t = new Task("", "t1", "one");
+        Task t = new Task();
+        t.setName("tast 1");
+        t.setDesc("task one");
         em.persist(t);
-        Task t2 = new Task("", "t2", "two");
-        em.persist(t2);
         tx.commit();
 
-        System.out.print("Hello world");
-        List l = em.createQuery("Select o FROM Task o").getResultList();
-
-
-        System.out.println(l.size());
-        System.out.print("Hello world");
+        Task fetched = em.find(Task.class, t.getId());
+        Assert.assertEquals(t.getName(), fetched.getName());
+        Assert.assertEquals(t.getDesc(), fetched.getDesc());
     }
 }
