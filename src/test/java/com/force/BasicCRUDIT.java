eb3813a61d0e6122a9b95b8c76f48306b1668b27
Nawab Iqbal <niqbal@salesforce.com>
2011-06-22 16:33:24 -0700

    renamed test file, so that it is picked for verify

--- src/test/java/com/force/BasicCRUDIT.java
+++ src/test/java/com/force/BasicCRUDIT.java
@@ -3,15 +3,15 @@ package com.force;
 
 import com.force.model.Task;
 import junit.framework.Assert;
+import junit.framework.TestCase;
 import org.junit.Test;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
-public class BasicCRUD {
+public class BasicCRUDIT extends TestCase {
 
     @Test
     public void testCRUD() {
